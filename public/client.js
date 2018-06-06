b8ef431bdd4b1e91f0fb718166e5255f20948874
elsistra <el@anon.systems>
2018-06-06 16:39:43 -0500

    Searches Fix

--- public/client.js
+++ public/client.js
@@ -1,30 +1,41 @@
 console.log('Hello');
 const socket = io();
 
-// Ask server for users data
-socket.emit('want-users-list');
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log('DOMContentLoaded');
+  if (document.getElementById('usersList')) {
+    // Ask server for users data
+    socket.emit('want-users-list');
 
-// Listen for users array replacement from server.
-socket.on('users-list', function (usersArray) {
-  var listElement = document.getElementById('usersList');
-  // For each user in Users array...
-  usersArray.forEach((user) => {
-    const newElement = document.createElement('li');
-    newElement.textContent = user.username;
-    listElement.appendChild(newElement);
-  });
-});
+    // Listen for users array replacement from server.
+    socket.on('users-list', function (usersArray) {
+      var listElement = document.getElementById('usersList');
+      if (listElement) {
+        // For each user in Users array...
+        usersArray.forEach((user) => {
+          const newElement = document.createElement('li');
+          newElement.textContent = user.username;
+          listElement.appendChild(newElement);
+        });
+      }
+    });
+  }
 
-// Ask server for searches data
-socket.emit('want-searches-list');
+  if (document.getElementById('searchesList')) {
+    // Ask server for searches data
+    socket.emit('want-searches-list');
 
-// Listen for users array replacement from server.
-socket.on('searches-list', function (searchesArray) {
-  var listElement = document.getElementById('searchesList');
-  // For each user in Users array...
-  searchesArray.forEach((search) => {
-    const newElement = document.createElement('li');
-    newElement.textContent = search.keyword;
-    listElement.appendChild(newElement);
-  });
+    // Listen for users array replacement from server.
+    socket.on('searches-list', function (searchesArray) {
+      var listElement = document.getElementById('searchesList');
+      if (listElement) {
+        // For each user in Users array...
+        searchesArray.forEach((search) => {
+          const newElement = document.createElement('li');
+          newElement.textContent = search.keyword;
+          listElement.appendChild(newElement);
+        });
+      }
+    });
+  }
 });
